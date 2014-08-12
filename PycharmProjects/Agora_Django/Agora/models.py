d0839eba88aca0cbb97e9d47e8dfa741804515f0
samsath <samhipwell@gmail.com>
2014-08-12 18:21:15 +0100

    Changes

---
+++ PycharmProjects/Agora_Django/Agora/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profiles(models.Model):
+    user =  models.OneToOneField(User)
+    blur = models.TextField()
+    photo = models.ImageField(upload_to='%Y/%m/%d', blank=True, null=True)
+    role = models.CharField(max_length=20)
+
+class Notification(models.Model):
+    userto = models.ManyToManyField(User, related_name='to')
+    userfrom = models.OneToOneField(User, related_name='from')
+    body = models.TextField()
+    datetofrom = models.DateTimeField()
